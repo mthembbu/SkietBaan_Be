68f22153d4271802c8a671402422298419e17eaa
Mandla <mandlamasombuka21@gmail.com>
2019-02-05 14:23:55 +0200

    Created user model

---
+++ SkietbaanBE/SkietbaanBE/Models/ModelsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SkietbaanBE.Models {
+    public class ModelsContext : DbContext {
+        public ModelsContext(DbContextOptions<ModelsContext> options)
+           : base(options) { }
+
+        //public ModelsContext() : base() { }
+
+        //Tables
+        public virtual DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ SkietbaanBE/SkietbaanBE/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkietbaanBE.Models {
+    public class User {
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string MemberID { get; set; }
+        public string Email { get; set; }
+        public DateTime MemberExpiry { get; set; }
+        public DateTime EntryDate { get; set; }
+        public bool Admin { get; set; }
+
+        public static implicit operator Task<object>(User v) {
+            throw new NotImplementedException();
+        }
+    }
+}
