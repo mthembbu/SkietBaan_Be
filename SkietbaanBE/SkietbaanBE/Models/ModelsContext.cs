81c10e83eab266569f9dce0f7a89a409d0d227ba
Mandla <mandlamasombuka21@gmail.com>
2019-02-13 10:04:27 +0200

    Updated models

--- SkietbaanBE/SkietbaanBE/Models/ModelsContext.cs
+++ SkietbaanBE/SkietbaanBE/Models/ModelsContext.cs
@@ -4,14 +4,22 @@ using System;
 
 namespace SkietbaanBE.Models {
     public class ModelsContext : DbContext {
+
         public ModelsContext(DbContextOptions<ModelsContext> options)
-           : base(options) { }
-        public ModelsContext() { }
+           : base(options) {
+            //Database.EnsureDeleted();
+            //Database.Migrate();
+        }
+
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Competition> Competitions { get; set; }
         public virtual DbSet<Score> Scores { get; set; }
         public virtual DbSet<Group> Groups { get; set; }
         public virtual DbSet<UserGroup> UserGroups { get; set; }
         public virtual DbSet<UserCompStats> UserCompStats { get; set; }
+
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+        }*/
     }
 }
