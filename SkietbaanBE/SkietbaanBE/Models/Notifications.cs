d1b9899842cad2033c12d50a64bf97d076749928
DinolanNaidoo <dnaidoo@retrorabbit.co.za>
2019-02-14 13:43:21 +0200

    Added Notifications.cs

---
+++ SkietbaanBE/SkietbaanBE/Models/Notifications.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkietbaanBE.Models
+{
+    public class Notifications
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public bool IsRead { get; set; }
+        public string NotificationHeading { get; set; }
+        public string NotificationContent { get; set; }
+        public virtual User User { get; set; }
+    }
+}
