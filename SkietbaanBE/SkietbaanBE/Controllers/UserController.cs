1b61631c36734114499ab42b17a137fc069cad7d
DinolanNaidoo <dnaidoo@retrorabbit.co.za>
2019-02-14 08:43:14 +0200

    so the code compiles

--- SkietbaanBE/SkietbaanBE/Controllers/UserController.cs
+++ SkietbaanBE/SkietbaanBE/Controllers/UserController.cs
@@ -31,8 +31,37 @@ namespace SkietbaanBE.Controllers
         {
             return await _context.Users.FindAsync(id);
         }
+        //POST: api/User
+        [HttpPost]
+        public async Task<IActionResult> AddUser([FromBody] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                User dbUser = null; //assume user does not exist
 
-        // POST: api/User
+                dbUser = _context.Users.FirstOrDefault(x => x.Username == user.Username);
+
+                //if user aready exist return
+                if (dbUser != null)
+                {
+                    return Ok("User already exists");
+                }
+                //get today's date and save it under user entry date
+                user.EntryDate = DateTime.Now;
+                //encrypt password
+                user.Password = Security.HashSensitiveData(user.Password);
+                //Save User
+                await _context.AddAsync(user);
+                await _context.SaveChangesAsync();
+
+                return Ok("User saved successfully");
+            }
+            else
+            {
+                return new BadRequestObjectResult("user cannot be null");
+            }
+        }
+        // PUT: api/User
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateUser(int id, [FromBody] User user)
         {
@@ -69,56 +98,7 @@ namespace SkietbaanBE.Controllers
             }
         }
     }
-        // PUT: api/User
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateUser(int id, [FromBody] User user)
-        {
-                //error handling, check if client provided valid data
-                if (user == null)
-                {
-                    return new BadRequestObjectResult("user cannot be null");
-                }
-                else
-                {
-                    User dbUser = _context.Users.Where(u => u.Username == user.Username && u.Id != user.Id) //check if a different user with the new username already exists
-                        .FirstOrDefault<User>();
-                    if (dbUser != null)
-                    {
-                        return BadRequest("Cannot update user, Username already exists");
-                    }
-                    dbUser = _context.Users.Where(u => u.Id == user.Id)
-                        .FirstOrDefault<User>();
-
-                    //now updating user details
-                    dbUser.MemberID = user.MemberID;
-                    dbUser.EntryDate = user.EntryDate;
-                    dbUser.MemberExpiry = user.MemberExpiry;
-                    _context.Users.Update(dbUser);
-                    await _context.SaveChangesAsync();
-                    return Ok("User update successful");
-                }
-            
-        }
-        // POST: api/user/login
-        [HttpPost("login")]
-        public async Task<ActionResult> LoginPost([FromBody]User user)
-        {
-            if (user.Username == null || user.Password == null || user.Email == null)
-            {
-                return new BadRequestObjectResult("No empty fields allowed");
-            }
-            foreach (User dbUser in GetUsers())
-            {
-                if (dbUser.Username.Equals(user.Username))
-                {
-                    if (Security.CompareHashedData(dbUser.Password,user.Password))
-                        return new OkObjectResult("Successful login");
-                    else
-                        return new BadRequestObjectResult("Incorrect Password or Username");
-                }
-            }
-            return new BadRequestObjectResult("User not found");
-        }
-    }
 }
+
+
 
