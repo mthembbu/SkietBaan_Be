d4402e1bb191a59eb6c4cae5ed88399ef47b50ad
Bathande <bndaba@retrorabbit.co.za>
2019-02-22 09:50:05 +0200

    took out all the white spaces

--- SkietbaanBE/SkietbaanBE/Controllers/GroupsController.cs
+++ SkietbaanBE/SkietbaanBE/Controllers/GroupsController.cs
@@ -13,22 +13,18 @@ namespace SkietbaanBE.Controllers
     [Route("api/Groups")]
     public class GroupsController : Controller
     {
-        EmailService mailS = new EmailService();
-        /*LinkCode _model = new LinkCode();*/
         private readonly ModelsContext _context;
 
         public GroupsController(ModelsContext context)
         {
             _context = context;
         }
-
         // GET: api/Groups
         [HttpGet]
         public IEnumerable<Group> GetGroups()
         {
             return _context.Groups;
         }
-
         // GET: api/Groups/5
         [HttpGet("{id}")]
         public async Task<IActionResult> GetGroup([FromRoute] int id)
@@ -37,17 +33,13 @@ namespace SkietbaanBE.Controllers
             {
                 return BadRequest(ModelState);
             }
-
             var @group = await _context.Groups.SingleOrDefaultAsync(m => m.Id == id);
-
             if (@group == null)
             {
                 return NotFound();
             }
-
             return Ok(@group);
         }
-
         // PUT: api/Groups/5
         [HttpPut("{id}")]
         public async Task<IActionResult> PutGroup([FromRoute] int id, [FromBody] Group @group)
@@ -56,14 +48,11 @@ namespace SkietbaanBE.Controllers
             {
                 return BadRequest(ModelState);
             }
-
             if (id != @group.Id)
             {
                 return BadRequest();
             }
-
             _context.Entry(@group).State = EntityState.Modified;
-
             try
             {
                 await _context.SaveChangesAsync();
@@ -79,10 +68,8 @@ namespace SkietbaanBE.Controllers
                     throw;
                 }
             }
-
             return NoContent();
         }
-
         // POST: api/Groups
         [HttpPost]
         public async Task<IActionResult> PostGroup([FromBody] Group @group)
@@ -91,10 +78,8 @@ namespace SkietbaanBE.Controllers
             {
                 return BadRequest(ModelState);
             }
-
             _context.Groups.Add(@group);
             await _context.SaveChangesAsync();
-
             return CreatedAtAction("GetGroup", new { id = @group.Id }, @group);
         }
 
@@ -106,13 +91,11 @@ namespace SkietbaanBE.Controllers
             {
                 return BadRequest(ModelState);
             }
-
             var @group = await _context.Groups.SingleOrDefaultAsync(m => m.Id == id);
             if (@group == null)
             {
                 return NotFound();
             }
-
             _context.Groups.Remove(@group);
             await _context.SaveChangesAsync();
 
@@ -123,11 +106,9 @@ namespace SkietbaanBE.Controllers
         {
             return _context.Groups.Any(e => e.Id == id);
         }
-
         [HttpPost]
         [Route("add")]
         public void AddListUsers([FromBody] List<User> users)
-            
         {
             Group group = (_context.Groups.ToArray())[_context.Groups.ToArray().Length - 1];
             List<UserGroup> userGroups = new List<UserGroup>();
@@ -135,14 +116,11 @@ namespace SkietbaanBE.Controllers
             for (int i = 0; i < users.Count; i++)
             {
                 UserGroup userGroup = new UserGroup();
-
                 User dbUser = _context.Users.FirstOrDefault(x => x.Username == users.ElementAt(i).Username);
                 userGroup.Group = group;
                 userGroup.User = dbUser;
-
                 userGroups.Add(userGroup);
             }
-
             _context.UserGroups.AddRange(userGroups);
             _context.SaveChanges();
         }
