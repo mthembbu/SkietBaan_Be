ecdc9383fd0ed2b66efb6c44591acd6f53c81763
Mandla <mandlamasombuka21@gmail.com>
2019-03-19 16:49:56 +0200

    Requirements table added

---
+++ SkietbaanBE/SkietbaanBE/Models/Requirement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkietbaanBE.Models {
+    public class Requirement {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public virtual Competition Competition { get; set; }
+        public string Standard { get; set; }
+        public double Accuracy { get; set; }
+        public double Total { get; set; }
+    }
+}
