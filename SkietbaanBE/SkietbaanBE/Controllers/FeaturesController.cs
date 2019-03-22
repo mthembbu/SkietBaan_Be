92e784409d57350237702e968ae5c71bfee266fa
Bathande <bndaba@retrorabbit.co.za>
2019-03-22 15:16:30 +0200

    made the orderby when getting users

--- SkietbaanBE/SkietbaanBE/Controllers/FeaturesController.cs
+++ SkietbaanBE/SkietbaanBE/Controllers/FeaturesController.cs
@@ -70,7 +70,7 @@ namespace SkietbaanBE.Controllers
         [ActionName("TimeLeft")]
         public IEnumerable<int> TimeLeft()
         {
-            var dbUsers = _context.Users.Where(u => u.MemberID != null && u.MemberID != "");
+            var dbUsers =( _context.Users.Where(u => u.MemberID != null && u.MemberID != "")).OrderBy(x=>x.Username);
             DateTime current = DateTime.Now;
             var months = new List<int>();
             foreach (var user in dbUsers)
