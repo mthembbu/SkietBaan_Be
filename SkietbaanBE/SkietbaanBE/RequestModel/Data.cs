991ccbb32f9099d4a16576ea1ebeab15ce5ef7bf
Mandla <mandlamasombuka21@gmail.com>
2019-04-25 13:44:54 +0200

    fix fix fix

---
+++ SkietbaanBE/SkietbaanBE/RequestModel/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkietbaanBE.RequestModel {
+    public class Data {
+        public string Label { get; set; }
+        public double Value { get; set; }
+        public string Description { get; set; }
+    }
+}
