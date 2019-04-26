0a37d11ca0181bed2d0e98adcde8476300434b19
Bathande <bndaba@retrorabbit.co.za>
2019-04-26 08:26:41 +0200

    added the delete of competition controller

--- SkietbaanBE/SkietbaanBE/Controllers/CompetitionController.cs
+++ SkietbaanBE/SkietbaanBE/Controllers/CompetitionController.cs
@@ -75,6 +75,27 @@ namespace SkietbaanBE.Controllers
             }
             
         }
+
+        // DELETE: api/Groups/5
+        [HttpPost("delete")]
+        public void DeleteGroup([FromBody] Competition[] Compobj)
+        {
+            for(int i = 0; i < Compobj.Length; i++)
+            {
+                var query = _context.Requirements.Where(m => m.Competition.Id == Compobj.ElementAt(i).Id);
+                if (query != null)
+                {
+                    foreach (var item in query)
+                    {
+                        _context.Requirements.Remove(item);
+                    }
+                    _context.SaveChanges(); 
+                }
+                _context.Competitions.Remove(Compobj.ElementAt(i));
+                _context.SaveChanges();
+            }
+    
+        }
         //posting the competition to the competition table together with a array of requirements using the Requirements filter
         // POST: api/Competition/filter
         [HttpPost("filter")]
