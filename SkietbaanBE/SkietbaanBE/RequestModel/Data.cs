89121564d063db134716e9bdbcd053fdbd7b5e4e
Mandla <mandlamasombuka21@gmail.com>
2019-05-03 19:22:37 +0200

    stats page

--- SkietbaanBE/SkietbaanBE/RequestModel/Data.cs
+++ SkietbaanBE/SkietbaanBE/RequestModel/Data.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SkietbaanBE.RequestModel {
+﻿namespace SkietbaanBE.RequestModel {
     public class Data {
         public string Label { get; set; }
         public double Value { get; set; }
+        public double GroupValue { get; set; }
         public string Description { get; set; }
     }
 }
